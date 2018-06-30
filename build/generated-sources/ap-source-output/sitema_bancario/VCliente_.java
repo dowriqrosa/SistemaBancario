7ad172648486912c3bfebb21c46ded6458109399
carlos henrique de oliveira santana rosa <31255323+dowriqrosa@users.noreply.github.com>
2018-06-30 11:35:13 -0300

    final
    final

---
+++ build/generated-sources/ap-source-output/sitema_bancario/VCliente_.java
@@ -0,0 +1,35 @@
+package sitema_bancario;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(VCliente.class)
+public abstract class VCliente_ {
+
+	public static volatile SingularAttribute<VCliente, Integer> Expedicao;
+	public static volatile SingularAttribute<VCliente, Character> Naturalidade;
+	public static volatile SingularAttribute<VCliente, Integer> id_client;
+	public static volatile SingularAttribute<VCliente, Character> Telefone;
+	public static volatile SingularAttribute<VCliente, Character> Complemento;
+	public static volatile SingularAttribute<VCliente, Character> Bairro;
+	public static volatile SingularAttribute<VCliente, Character> Emissor;
+	public static volatile SingularAttribute<VCliente, Character> Cidade;
+	public static volatile SingularAttribute<VCliente, Character> Estado;
+	public static volatile SingularAttribute<VCliente, Character> Cep;
+	public static volatile SingularAttribute<VCliente, Integer> Nascimento;
+	public static volatile SingularAttribute<VCliente, Character> Mae;
+	public static volatile SingularAttribute<VCliente, Character> Nacionalidade;
+	public static volatile SingularAttribute<VCliente, Integer> Rg;
+	public static volatile SingularAttribute<VCliente, Character> Profissao;
+	public static volatile SingularAttribute<VCliente, Integer> Cpf;
+	public static volatile SingularAttribute<VCliente, Character> Pai;
+	public static volatile SingularAttribute<VCliente, Character> Nome;
+	public static volatile SingularAttribute<VCliente, Character> sexo;
+	public static volatile SingularAttribute<VCliente, Character> EstadoCivil;
+	public static volatile SingularAttribute<VCliente, Character> email;
+	public static volatile SingularAttribute<VCliente, Character> Rua;
+
+}
+
